e82661d5638d169ecb9b23558da366f125882e2a
Goutham <gouthamrs4u@gmail.com>
2018-05-17 19:01:09 +0530

    Setup done  for  image picker module both in android and ios

--- android/app/src/main/java/com/navigation_sample/MainApplication.java
+++ android/app/src/main/java/com/navigation_sample/MainApplication.java
@@ -3,6 +3,7 @@ package com.navigation_sample;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new SplashScreenReactPackage(),
             new ReactNativeConfigPackage(),
             new VectorIconsPackage()
--- screens/MediaScreen.js
+++ screens/MediaScreen.js
@@ -1,32 +1,104 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Icon } from 'native-base';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  PixelRatio,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 
-import Header from '../components/Header';
+import ImagePicker from 'react-native-image-picker';
 
-class MediaScreen extends Component {
+class ImagePickerProject extends React.Component {
+
+    state = {
+
+      ImageSource: null,
+
+    };
+
+    selectPhotoTapped() {
+      const options = {
+        quality: 1.0,
+        maxWidth: 500,
+        maxHeight: 500,
+        storageOptions: {
+          skipBackup: true
+        }
+      };
+
+      ImagePicker.showImagePicker(options, (response) => {
+        console.log('Response = ', response);
+
+        if (response.didCancel) {
+          console.log('User cancelled photo picker');
+        }
+        else if (response.error) {
+          console.log('ImagePicker Error: ', response.error);
+        }
+        else if (response.customButton) {
+          console.log('User tapped custom button: ', response.customButton);
+        }
+        else {
+          let source = { uri: response.uri };
+
+          // You can also display the image using data:
+          // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+          this.setState({
+
+            ImageSource: source
+
+          });
+        }
+      });
+    }
 
     render() {
-       return (
-           <View style={{flex: 1}}>
-            <Header navigation={this.props.navigation}  title={'Media'}/>
-               <View style={styles.container}>
-               <Text>MediaScreen</Text>
-               <Text>MediaScreen</Text>
-               <Text>MediaScreen</Text>
-               <Text>MediaScreen</Text>
-              </View>
-           </View>
-       )
-    }
-}
+      return (
+        <View style={styles.container}>
 
-const styles = StyleSheet.create({
-      container: {
-         flex: 1,
-         justifyContent: 'center',
-         alignItems: 'center'
-    }
-})
+          <TouchableOpacity onPress={this.selectPhotoTapped.bind(this)}>
 
-export default MediaScreen;
+            <View style={styles.ImageContainer}>
+
+            { this.state.ImageSource === null ? <Text>Select a Photo</Text> :
+              <Image style={styles.ImageContainer} source={this.state.ImageSource} />
+            }
+
+            </View>
+
+          </TouchableOpacity>
+
+        </View>
+      );
+    }
+
+  }
+
+  const styles = StyleSheet.create({
+
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#FFF8E1'
+    },
+
+    ImageContainer: {
+      borderRadius: 10,
+      width: 250,
+      height: 250,
+      borderColor: '#9B9B9B',
+      borderWidth: 1 / PixelRatio.get(),
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#CDDC39',
+
+    },
+
+  });
+
+export default ImagePickerProject;
